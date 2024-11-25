95b56ed0769b79d908da275f57cfd856e4f32eea
Ivan Cardoso <ivancmcosta@tecgraf.puc-rio.br>
2024-11-25 10:02:24 -0300

    ultimas alterações

--- src/logic/firebase/config/app.ts
+++ src/logic/firebase/config/app.ts
@@ -1,9 +1,14 @@
-import { initializeApp, FirebaseApp } from 'firebase/app'
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { initializeFirestore } from "firebase/firestore";
 
 const app: FirebaseApp = initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-})
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+});
 
-export { app }
+const db = initializeFirestore(app, {
+  experimentalAutoDetectLongPolling: true,
+});
+
+export { app, db };
